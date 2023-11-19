874c6066263dbcce206e8899e41a76b23dfc1918
Alex Sibrin <alex.uiweb@gmail.com>
2023-11-19 20:03:28 +0300

    fix launching process of the app

--- cmd/main.go
+++ cmd/main.go
@@ -2,29 +2,41 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
-	"runbot-auth/internal/api/rest/v1"
+	"runbot-auth/internal/api/rest"
 	"runbot-auth/internal/config"
 	"sync"
 	"syscall"
 )
 
 func main() {
-
-	conf, err := config.NewConfig()
+	log.Println("App is starting the initialization...")
+	// Init config
+	conf, err := config.NewConfig(config.EnvInitKey)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	server, err := rest.NewHttpServer()
+	// <- init logger
+	defer log.Println("App is stopped.")
+
+	// <- init db, cache
+
+	// <-- init controllers, middlewares, router
+
+	// Init http server
+	server, err := rest.NewHttpServer(&rest.DependenciesHttpServer{
+		Config: conf.HttpServer,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	erchan := make(chan error)
-
+	// Init graceful shutdown
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
@@ -33,6 +45,18 @@ func main() {
 	wg.Add(1)
 	go func() {
 		server.Run(ctx)
+		wg.Done()
 	}()
 
+	log.Println("App is running.")
+	<-ctx.Done()
+
+	if !errors.Is(ctx.Err(), context.Canceled) {
+		err := context.Cause(ctx)
+		log.Fatal(fmt.Sprintf("App is crushed: %s", err.Error()))
+	}
+
+	log.Println("Services are stopping. Please wait...")
+
+	wg.Wait()
 }
