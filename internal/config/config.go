8d713d0a2a17aa09bafe48f6c73fdb499243b774
Alex Sibrin <alex.uiweb@gmail.com>
2023-11-19 20:01:30 +0300

    add parameter to NewConfig for an various initializaion config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,20 @@
 package config
 
+import "errors"
+
+const (
+	EnvInitKey  = "env"
+	YamlInitKey = "yaml"
+)
+
+var (
+	ErrWrongInitKey = errors.New("wrong config init key")
+)
+
 type Config struct {
-	PostgreSQL
-	Redis
-	HttpServer
+	*PostgreSQL
+	*Redis
+	*HttpServer
 }
 
 type PostgreSQL struct {
@@ -16,6 +27,22 @@ type HttpServer struct {
 	Addr string
 }
 
-func NewConfig() (*Config, error) {
+func NewConfig(key string) (config *Config, err error) {
+	switch key {
+	case EnvInitKey:
+		config, err = initByEnvKey()
+	case YamlInitKey:
+		config, err = initByYamlKey()
+	default:
+		config, err = nil, ErrWrongInitKey
+	}
+	return config, err
+}
+
+func initByYamlKey() (*Config, error) {
+	return nil, nil
+}
+
+func initByEnvKey() (*Config, error) {
 	return nil, nil
 }
