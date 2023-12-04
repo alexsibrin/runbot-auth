72063f7d20e04142010731b9f42756e384c42a4a
Alex Sibrin <alex.uiweb@gmail.com>
2023-12-04 22:12:01 +0300

    add inner models

--- internal/services/auth.go
+++ internal/services/auth.go
@@ -2,19 +2,38 @@ package services
 
 import "context"
 
+type SignInModel struct {
+	Email, Password string
+}
+
+type SignUpModel struct {
+	Email, Password string
+	Name            string
+}
+
+type IAuthRepo interface {
+	InsertUser()
+}
+
+type ITokenRepo interface {
+	AddToken(string) error
+}
+
 type DependenciesAuth struct {
 }
 
 type Auth struct {
+	authrepo  IAuthRepo
+	tokenrepo ITokenRepo
 }
 
 func NewAuth(deb *DependenciesAuth) (*Auth, error) {
 	return nil, nil
 }
 
-func (s *Auth) SignUp(ctx context.Context) {
-
+func (s *Auth) SignUp(ctx context.Context) (string, error) {
+	return "", nil
 }
-func (s *Auth) LogIn()        {}
-func (s *Auth) CreateToken()  {}
-func (s *Auth) RefreshToken() {}
+func (s *Auth) LogIn(ctx context.Context) (string, error) {}
+func (s *Auth) CreateToken()                              {}
+func (s *Auth) RefreshToken()                             {}
