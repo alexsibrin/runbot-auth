eb3d596b9d1a2d5814a26032be77cb5877f171b0
Alex Sibrin <alex.uiweb@gmail.com>
2023-12-20 21:50:48 +0300

    add Register, Get and convertAccountGetModelToResponse methods

--- internal/api/rpc/handlers/account.go
+++ internal/api/rpc/handlers/account.go
@@ -1,11 +1,48 @@
 package handlers
 
-import "runbot-auth/internal/api/models"
+import (
+	"context"
+	"google.golang.org/grpc"
+	"runbot-auth/internal/api/models"
+	"runbot-auth/pkg/runbotauthproto"
+)
 
 type IController interface {
-	GetAccount(uuid string) (*models.Account, error)
-	UpdateAccount(update *models.UpdateAccount) error
+	GetOne(ctx context.Context, uuid string) (*models.AccountGet, error)
 }
 
-type AccountService struct {
+type AccountDependencies struct {
+	Controller IController
+}
+
+type Account struct {
+	controller IController
+	runbotauthproto.UnimplementedAccountServer
+}
+
+func NewAccount(d *AccountDependencies) (*Account, error) {
+	return &Account{
+		controller: d.Controller,
+	}, nil
+}
+
+func (h *Account) Register(s *grpc.Server) {
+	runbotauthproto.RegisterAccountServer(s, h)
+}
+
+func (h *Account) Get(ctx context.Context, model *runbotauthproto.GetAccount) (*runbotauthproto.GetAccountResponse, error) {
+	modelout, err := h.controller.GetOne(ctx, model.UUID)
+	if err != nil {
+		return nil, err
+	}
+	response := h.convertAccountGetModelToResponse(modelout)
+	return response, nil
+}
+
+func (h *Account) convertAccountGetModelToResponse(model *models.AccountGet) *runbotauthproto.GetAccountResponse {
+	return &runbotauthproto.GetAccountResponse{
+		UUID:  model.UUID,
+		Name:  model.Name,
+		Email: model.Email,
+	}
 }
