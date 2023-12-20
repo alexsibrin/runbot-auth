2c7484a49ad528217dde507b9cc9f16910b830e6
Alex Sibrin <alex.uiweb@gmail.com>
2023-12-20 21:49:37 +0300

    add Run method

--- internal/api/rpc/server.go
+++ internal/api/rpc/server.go
@@ -1,25 +1,51 @@
 package rpc
 
-import "google.golang.org/grpc"
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"net"
+)
 
 type IServiceRegister interface {
-	Register(*Server)
+	Register(*grpc.Server)
 }
 
 type Config struct {
+	Port int
 }
 
 type Server struct {
 	server *grpc.Server
+	config *Config
 }
 
 func NewServer(c *Config) (*Server, error) {
 	server := grpc.NewServer()
 	return &Server{
 		server: server,
+		config: c,
 	}, nil
 }
 
 func (s *Server) Add(service IServiceRegister) {
-	service.Register(s)
+	service.Register(s.server)
+}
+
+func (s *Server) Run(ctx context.Context) {
+	ctx, cause := context.WithCancelCause(ctx)
+	list, err := net.Listen("tcp", fmt.Sprintf(":%d", s.config.Port))
+	if err != nil {
+		cause(err)
+		return
+	}
+
+	err = s.server.Serve(list)
+	if err != nil {
+		cause(err)
+		return
+	}
+	
+	<-ctx.Done()
+	s.server.GracefulStop()
 }
