c2c669469b2e042376ef773dea8fc69de0b6669b
Alex Sibrin <alex.uiweb@gmail.com>
2024-01-23 22:11:11 +0300

    init logger

--- internal/logapp/logapp.go
+++ internal/logapp/logapp.go
@@ -1,11 +1,30 @@
 package logapp
 
+import (
+	"github.com/sirupsen/logrus"
+	"os"
+	"time"
+)
+
 type ILogger interface {
+	logrus.FieldLogger
 }
 
-type Logger struct {
+type Config struct {
+	Level         int
+	Colors        bool
+	FullTimestamp bool
 }
 
-func NewLogger() {
+func NewLogger(config *Config) ILogger {
+	l := logrus.New()
+	l.SetFormatter(&logrus.TextFormatter{
+		ForceColors:     config.Colors,
+		FullTimestamp:   config.FullTimestamp,
+		TimestampFormat: time.RFC3339,
+	})
+	l.SetOutput(os.Stdout)
+	l.SetLevel(logrus.Level(config.Level))
 
+	return l
 }
