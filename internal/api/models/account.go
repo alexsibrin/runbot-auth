78291f5e2e1751dd27f3cc5b8eb896a5f99717b4
Alex Sibrin <alex.uiweb@gmail.com>
2024-02-04 17:01:45 +0300

    change UpdatedAt to json ignoring if it's empty, add Marshal to the Token model so it returns anyl access token to the response

--- internal/api/models/account.go
+++ internal/api/models/account.go
@@ -1,6 +1,33 @@
 package models
 
+import "encoding/json"
+
 // The creating of an account
+
+// SignUp the input model for the signing up
+type SignUp struct {
+	Email    string
+	Password string
+	Name     string
+}
+
+// SignUpResponse the response for the successful signing up
+type SignUpResponse struct {
+	Account *Account
+	Token   *Token
+}
+
+// SignIn is a model for signing in
+type SignIn struct {
+	Email    string
+	Password string
+}
+
+// SignIn the response for the successful signing in
+type SignInResponse struct {
+	Account *Account
+	Token   *Token
+}
 
 // AccountCreate the input model for the creating an accunt
 type AccountCreate struct {
@@ -22,7 +49,7 @@ type Account struct {
 	Password  string
 	Name      string
 	CreatedAt int64
-	UpdatedAt int64
+	UpdatedAt int64 `json:"UpdatedAt,omitempty"`
 }
 
 // AccountGet input model for a getting an account by UUID
@@ -34,7 +61,7 @@ type AccountGetModel struct {
 	Email     string
 	Name      string
 	CreatedAt int64
-	UpdatedAt int64
+	UpdatedAt int64 `json:"UpdatedAt,omitempty"`
 }
 
 // UpdateAccount input model for an updating an account
@@ -44,14 +71,12 @@ type UpdateAccount struct {
 	Phone string
 }
 
-// SignIn is a model for validating the user
-type SignIn struct {
-	Email    string
-	Password string
-}
-
 // Token the general token model
 type Token struct {
 	Access  string
-	Refresh string
+	Refresh string `json:"-"`
+}
+
+func (t Token) MarshalJSON() ([]byte, error) {
+	return json.Marshal(t.Access)
 }
