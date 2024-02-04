c5832c60a348b85f2efcff9e10684594838342a6
Alex Sibrin <alex.uiweb@gmail.com>
2024-02-04 16:46:05 +0300

    add jwt, common, and init with env variables method

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,10 +3,15 @@ package config
 import (
 	"errors"
 	"github.com/spf13/viper"
+	"log"
+	"os"
+	"strings"
+	"time"
 )
 
 const (
 	EnvInitKey = "env"
+	envPref    = "APP"
 
 	YamlInitKey = "yaml"
 	configPath  = "."
@@ -21,7 +26,10 @@ var (
 type Config struct {
 	PostgreSQL
 	RestServer
+	GRPCServer
 	Logger
+	Jwt
+	Common
 }
 
 type PostgreSQL struct {
@@ -30,12 +38,16 @@ type PostgreSQL struct {
 	Port     string
 	User     string
 	Password string
-	SSLMode  bool
+	SSLMode  string
 }
 
 type RestServer struct {
 	Host string
 	Port string
+}
+
+type GRPCServer struct {
+	Port int
 }
 
 type Logger struct {
@@ -44,7 +56,18 @@ type Logger struct {
 	FullTimestamp bool
 }
 
-// TODO: Use flags here OR maybe in the main
+type Jwt struct {
+	Salt      string
+	Issuer    string
+	Subject   string
+	Audience  []string
+	ExpiresIn time.Duration
+}
+
+type Common struct {
+	Version string
+	Health  string
+}
 
 func New(key string) (config *Config, err error) {
 	switch key {
@@ -77,7 +100,24 @@ func initByYamlKey() (*Config, error) {
 	return &c, nil
 }
 
+// FIXME: Waiting when viper will get ability to map env vars to a struct directly without kostils
 func initByEnvKey() (*Config, error) {
-	// TODO: Complete me
-	return nil, ErrConfigInit
+	var cfg Config
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+
+	for _, s := range os.Environ() {
+		replacer := strings.NewReplacer("_", ".")
+		envToBind := replacer.Replace(strings.Split(s, "=")[0])
+		if err := viper.BindEnv(envToBind); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	err := viper.Unmarshal(&cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
 }
