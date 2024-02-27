4709e58fa9b9a4d9e9a8b1cc23504c562faa62f1
Alex Sibrin <alex.uiweb@gmail.com>
2024-02-27 21:47:08 +0300

    add errorhandler method

--- internal/api/rpc/handlers/account.go
+++ internal/api/rpc/handlers/account.go
@@ -4,10 +4,16 @@ import (
 	"context"
 	"errors"
 	"github.com/alexsibrin/runbot-auth/internal/api/models"
+	"github.com/alexsibrin/runbot-auth/internal/api/validators"
 	"github.com/alexsibrin/runbot-auth/internal/logapp"
+	"github.com/alexsibrin/runbot-auth/internal/repositories"
 	"github.com/alexsibrin/runbot-auth/pkg/runbotauthproto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+// TODO: Add error handling with status package
 
 const (
 	accountKey = "accounts"
@@ -60,15 +66,27 @@ func (h *Account) Register(s *grpc.Server) {
 func (h *Account) Get(ctx context.Context, model *runbotauthproto.GetAccount) (*runbotauthproto.GetAccountResponse, error) {
 	modelout, err := h.controller.GetOneByUUID(ctx, model.UUID)
 	if err != nil {
-		h.handlerError(err)
-		return nil, err
+		return nil, h.handlerError(err)
 	}
 	response := h.convertAccountGetModelToResponse(modelout)
 	return response, nil
 }
 
-func (h *Account) handlerError(err error) {
+func (h *Account) handlerError(err error) error {
 	h.logger.Error(err)
+
+	s := codes.Internal
+
+	switch {
+	case errors.Is(err, validators.ErrEmailIsTooShort):
+		s = codes.Canceled
+	case errors.Is(err, validators.ErrEmailFormatIsNotCorrect):
+		s = codes.Canceled
+	case errors.As(err, &repositories.ErrAccountNotFoundByUUID{}):
+		s = codes.NotFound
+	}
+
+	return status.Error(s, err.Error())
 }
 
 func (h *Account) convertAccountGetModelToResponse(model *models.AccountGetModel) *runbotauthproto.GetAccountResponse {
