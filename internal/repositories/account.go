998123f6196bf77a770a1c762b09ca1e9ea5492d
Alex Sibrin <alex.uiweb@gmail.com>
2024-02-27 21:47:52 +0300

    fix err names

--- internal/repositories/account.go
+++ internal/repositories/account.go
@@ -2,16 +2,29 @@ package repositories
 
 import "fmt"
 
-type ErrAccountNotFound struct {
+// TODO: Move errors to the usecase OR errorspkg?
+type ErrAccountNotFoundByEmail struct {
 	email string
 }
 
-func (err ErrAccountNotFound) Error() string {
+func (err ErrAccountNotFoundByEmail) Error() string {
+	return fmt.Sprintf("account with email=%s is not found", err.email)
+}
+
+func NewErrAccountNotFoundByEmail(email string) error {
+	return ErrAccountNotFoundByEmail{email}
+}
+
+type ErrAccountNotFoundByUUID struct {
+	email string
+}
+
+func (err ErrAccountNotFoundByUUID) Error() string {
 	return fmt.Sprintf("account with UUID=%s is not found", err.email)
 }
 
-func NewErrAccountNotFound(uuid string) error {
-	return ErrAccountNotFound{uuid}
+func NewErrAccountNotFoundByUUID(uuid string) error {
+	return ErrAccountNotFoundByUUID{uuid}
 }
 
 type Account struct {
