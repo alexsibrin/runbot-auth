32e18e9e6ebcf6c9ff9aaba5a8ea1ba681007c59
Alex Sibrin <alex.uiweb@gmail.com>
2024-03-10 00:27:51 +0300

    add statuses

--- internal/entities/account.go
+++ internal/entities/account.go
@@ -1,10 +1,26 @@
 package entities
 
+const (
+	Active uint8 = iota
+	Suspended
+	Blocked
+)
+
 type Account struct {
 	UUID      string
 	Email     string
 	Password  string
 	Name      string
+	Status    uint8
 	CreatedAt int64
 	UpdatedAt int64
+}
+
+func (e *Account) IsActive() bool {
+	switch e.Status {
+	case Active:
+		return true
+	default:
+		return false
+	}
 }
