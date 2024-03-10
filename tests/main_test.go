0c866b0d11001062c026823ca943dfb4b27498f9
Alex Sibrin <alex.uiweb@gmail.com>
2024-03-10 19:09:40 +0300

    clear

--- tests/main_test.go
+++ tests/main_test.go
@@ -7,11 +7,6 @@ import (
 )
 
 func TestSmt(t *testing.T) {
-	s, err := exec.LookPath("Skillbox - DevOps-инженер. Основы")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(s)
 	b, err := exec.Command("ls", "-la").Output()
 	if err != nil {
 		fmt.Println(err)
